1f209eb1f730fab39f8a9eb14b6cfb3b23de80f5
Jereh Lomak <jerehlomak@gmail.com>
2026-04-26 09:13:44 +0100

    fix: remove temp scripts breaking vercel build

--- get-picklist.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config({path: '.env.local'});
-const { sfGetPicklistValues } = require('./lib/salesforce/client');
-
-sfGetPicklistValues('Lead', 'Region__c').then(console.log).catch(console.error);
